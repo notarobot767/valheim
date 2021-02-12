e5ddb534706c94ac855aa5eb94bee34ebcccb40d
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-02-11 19:56:17 -0500

    added more comments to update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #Order of operation
 #  cd to directory relative to script
 #  source variables from ./config.conf
-#  pull latest steam image
+#  pull latest steamcmd image
 #  stop valheim server container instance if running
 #  run update in a temporal container
 
@@ -16,6 +16,8 @@ source ./config.conf
 IMAGE=$STEAM
 NAME=$VALHEIM
 #point name and image to vars from config.conf
+#we could just use the vars from config.conf, but
+  #i prefer to be consistent in my scripts with the words image and name
 
 #SteamCMD update script
 #######################
@@ -31,10 +33,28 @@ podman pull $IMAGE
 
 podman stop $NAME
 #stop any instance of the server container
-#in run.sh, the server will be name will be linked to $VALHEIM
+  #in run.sh, the server will be name will be linked to $VALHEIM
 
 podman run -it \
   --rm \
   -v $LOCAL_DATA:$REMOTE_DATA \
   -v $LOCAL_UPDATE_SCRIPT:$REMOTE_UPDATE_SCRIPT:ro \
   $IMAGE +runscript $REMOTE_UPDATE_SCRIPT
+#-it
+  #the process should not take long depending on your bandwidth, so
+  #interactive mode will allow you see the progress and any errors
+  #if you prefer it to be headless, just change -it to -d
+#--rm
+  #sole purpose of container image is to update our local server data,
+  #so no need to keep the container after it finishes.
+#-v $LOCAL_DATA:$REMOTE_DATA
+  #this mounts our local server data folder into the perceived container location.
+  #purely out of habbit, i assigned the remote destination to be /app from config.conf
+#-v $LOCAL_UPDATE_SCRIPT:$REMOTE_UPDATE_SCRIPT:ro
+  #this mounts a file for the steamcmd +runscript file.
+  #we could type out the arguments here instead, but i prefer to use a script.
+  #in order to use the script, we have to get the file into the container for
+  #steamcmd to find it, since the relative filesystem for steamcmd will be the container instance
+#$IMAGE +runscript $REMOTE_UPDATE_SCRIPT
+  #this calls the image to instanciate (steamcmd) and then run the following commands
+  #+runscript and it's script file on the container
