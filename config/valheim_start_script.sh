dd86311bc0f02e6b7d30af50e9298acf3c13ce1b
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-02-12 19:08:12 -0500

    added variable for game dir

--- config/valheim_start_script.sh
+++ config/valheim_start_script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+NAME="TEST SERVER"
+PORT="2456"
+WORLD="Dedicated"
+PW="password"
+DATA="/app/valheim"
+SAVE_DIR="$DATA/_config"
+
 #cd "${0%/*}"/valheim
-cd /app/valheim
-#change directory relative to script
+cd $DATA
+#change directory to location of game server files
 
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
@@ -11,12 +18,6 @@ export SteamAppId=892970
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-
-NAME="TEST SERVER"
-PORT="2456"
-WORLD="Dedicated"
-PW="password"
-SAVE_DIR="/app/_config"
 
 echo "Starting server PRESS CTRL-C to exit"
 
