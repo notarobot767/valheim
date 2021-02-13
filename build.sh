ade3f33d2b19bd80157a1850288bc6b2425a0239
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-02-12 19:22:49 -0500

    moved name to variable in config.conf

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 cd "${0%/*}"
 #change directory relative to script
 
+source ./config/config.conf
+
 podman build -f ./config/dockerfile.conf \
-        -t valheim \
+        -t $VALHEIM_IMAGE_NAME \
         .
+
+#invoke the podman build command on the docker file in the config folder
