37ac5336373ebfe84b9254382da5a1bb4c400b69
notarobot767 <12644755+notarobot767@users.noreply.github.com>
2023-06-05 23:09:53 -1000

    inital push

---
+++ data/start_server_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+echo "Starting server PRESS CTRL-C to exit"
+
+DIR="/app/data"
+if [ ! -d "$DIR" ]; then
+  mkdir $DIR
+fi
+#create worlds folder in /app folder
+#personal preference, avoids another volume mount in yml service
+
+./valheim_server.x86_64 -nographics -batchmode -savedir $DIR "$@"
+
+export LD_LIBRARY_PATH=$templdpath
+#Why does this come last? export does need to be last though for server to work
